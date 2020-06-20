945a3e29094e0871de66b6d3f0624c3174e63792
El-lobo-cimarron <62679040+El-lobo-cimarron@users.noreply.github.com>
2020-06-20 14:49:37 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,53 +1,67 @@
-// Text input appear in the grey window
+window.onload = function() {
 
-function translateText(){
+  // button
+  document.getElementById("translateButton").addEventListener("click", function() {
+    translateText();
+  });
+};
+function translateText() {
 
-  var textbox = document.getElementById("inputField");
-  document.getElementById("outputText").innerHTML = textbox.value;
+  var usrInput = document.getElementById("inputField").value; //What user typed in
+  var copy = usrInput.toLowerCase(); //Convert text to toLowerCase
 
-  var usrInput = textbox.value;
+  var outputText = document.getElementById("outputText");
 
-  // Symbols
-  function morseAlphabetSearch(){
-    var letters = {
-      a: "._",
-      b: "_...",
-      c: "_._.",
-      d: "_..",
-      e: ".",
-      f: ".._.",
-      g: "__.",
-      h: "....",
-      i: "..",
-      j: ".___",
-      k: "_._",
-      l: "._..",
-      m: "__",
-      n: "_.",
-      o: "___",
-      p: ".__.",
-      q: "__._",
-      r: "._.",
-      s: "...",
-      t: "_",
-      u: ".._",
-      v: "..._",
-      w: ".__",
-      x: "_.._",
-      y: "_.__",
-      z: "__.."
+  var letters = {
+    a: "._",
+    b: "_...",
+    c: "_._.",
+    d: "_..",
+    e: ".",
+    f: ".._.",
+    g: "__.",
+    h: "....",
+    i: "..",
+    j: ".___",
+    k: "_._",
+    l: "._..",
+    m: "__",
+    n: "_.",
+    o: "___",
+    p: ".__.",
+    q: "__._",
+    r: "._.",
+    s: "...",
+    t: "_",
+    u: ".._",
+    v: "..._",
+    w: ".__",
+    x: "_.._",
+    y: "_.__",
+    z: "__..",
+    1: ".____",
+    2: "..___",
+    3: "...__",
+    4: "...._",
+    5: ".....",
+    6: "_....",
+    7: "__...",
+    8: "___..",
+    9: "____.",
+    0: "_____"
 
-    };
+  };
+  var values = Object.values(letters);
 
-      // alert(values[]);
+  var text = "";
+
+  // looping through the letters
+  for (var i = 0; i < copy.length; i++) {
+    if (!(copy[i] in letters)) { //loops through the
+      text += "+";
+      continue;
+    }
+    text += letters[copy[i]] + "/";
   }
-var values = Object.values(letters);
-
-  morseAlphabet();
+  outputText.innerHTML = text;
 }
-// // looping through the letters
-// for (var i = 0; i < usrInput.length; i++) {
-//   if (values === usrInput[i]){
-//
-//   }
-// }
